0a677431cbc5480eaf38be1c5f67ae7b5e6c9607
Sreekanth K <sreekanth100@gmail.com>
2020-07-10 12:03:29 +0530

    A dummy book mark indicator implemented

--- app/src/main/java/com/embibe/tmdbapp/MovieAdapter.java
+++ app/src/main/java/com/embibe/tmdbapp/MovieAdapter.java
@@ -2,6 +2,7 @@ package com.embibe.tmdbapp;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
 import androidx.paging.PagedListAdapter;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,8 +25,6 @@ import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.embibe.tmdbapp.service.models.Movie;
-
-import java.util.ArrayList;
 
 public class MovieAdapter extends PagedListAdapter<Movie, MovieAdapter.MovieViewHolder> {
     private Context mContext;
@@ -55,10 +56,11 @@ public class MovieAdapter extends PagedListAdapter<Movie, MovieAdapter.MovieView
 
             Glide.with(mContext)
                     .load(thumbNailUrl)
-                    .into(holder.imageView);
+                    .into(holder.photoIv);
         }else{
             Toast.makeText(mContext, "Item is null", Toast.LENGTH_LONG).show();
         }
+
         String thumbNailUrl = "https://image.tmdb.org/t/p/original/" +movieObj.getPoster_path();
 
         CircularProgressDrawable circularProgressDrawable = new CircularProgressDrawable(mContext);
@@ -78,13 +80,40 @@ public class MovieAdapter extends PagedListAdapter<Movie, MovieAdapter.MovieView
             public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
 
 
-                holder.imageView.setImageDrawable(resource);
+                holder.photoIv.setImageDrawable(resource);
 
                 return false;
             }
 
-        }).into(holder.imageView);
+        }).into(holder.photoIv);
 
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            public void onClick(View v) {
+                Drawable bookMarkSelected = ContextCompat.getDrawable(mContext, R.drawable.ic_id_bookmark_selected);
+
+                final int sdk = android.os.Build.VERSION.SDK_INT;
+                if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                    holder.bookMarkIv.setBackgroundDrawable( bookMarkSelected );
+                } else {
+                    holder.bookMarkIv.setBackground( bookMarkSelected);
+                }
+
+
+
+            }
+        };
+
+        Drawable bookMarkUnSelected = ContextCompat.getDrawable(mContext, R.drawable.ic_id_bookmark_unselected);
+
+        final int sdk = android.os.Build.VERSION.SDK_INT;
+        if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            holder.bookMarkIv.setBackgroundDrawable( bookMarkUnSelected );
+        } else {
+            holder.bookMarkIv.setBackground( bookMarkUnSelected);
+        }
+
+
+        holder.bookMarkIv.setOnClickListener(onClickListener);
     }
 
     @Override
@@ -108,12 +137,14 @@ public class MovieAdapter extends PagedListAdapter<Movie, MovieAdapter.MovieView
     class MovieViewHolder extends RecyclerView.ViewHolder {
 
         TextView titleTv;
-        ImageView imageView;
+        ImageView photoIv;
+        ImageView bookMarkIv;
 
         public MovieViewHolder(View itemView) {
             super(itemView);
             titleTv         =   itemView.findViewById(R.id.id_title_tv);
-            imageView       =   itemView.findViewById(R.id.id_iv);
+            photoIv         =   itemView.findViewById(R.id.id_photo_iv);
+            bookMarkIv      =   itemView.findViewById(R.id.id_book_mark_iv);
         }
     }
 }
